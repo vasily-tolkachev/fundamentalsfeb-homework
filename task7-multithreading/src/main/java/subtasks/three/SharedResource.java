89d7933f4cd6359e5a7966bc8ec7368d8de9b0f4
vasily-tolkachev <vasilytolkachew@gmail.com>
2018-04-19 15:32:28 +0300

    task7 subtask3 initial

---
+++ task7-multithreading/src/main/java/subtasks/three/SharedResource.java
@@ -0,0 +1,23 @@
+package subtasks.three;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SharedResource {
+    private List<Integer> list;
+
+    public SharedResource() {
+        list = new ArrayList<>();
+    }
+
+    public void setElement(Integer element) {
+        list.add(element);
+    }
+
+    public Integer getElement() {
+        if (!list.isEmpty()) {
+            return list.remove(0);
+        }
+        return null;
+    }
+}
