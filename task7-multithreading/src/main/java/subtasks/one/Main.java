3bbad195f8aae082f3649cc67e17c1a6bf1e6bad
vasily-tolkachev <vasilytolkachew@gmail.com>
2018-04-21 21:36:41 +0300

    task7 subtask1 add generation of operations

--- task7-multithreading/src/main/java/subtasks/one/Main.java
+++ task7-multithreading/src/main/java/subtasks/one/Main.java
@@ -11,21 +11,49 @@ public class Main {
     public static void main(String[] args) {
         AccountManager accountManager = new AccountManager();
         final int accountNumber = 10;
+        final long balanceLimitTestValue = 100000;
+        final int transactionLimitTestValue = 10000;
+
         Set<Long> accountIdList = new HashSet<>();
         for (int i = 0; i < accountNumber; i++) {
-            long accountId = accountManager.createNewAccount(generateInitialBalance());
+            long accountId = accountManager.createNewAccount(generateInitialBalance(balanceLimitTestValue));
             accountIdList.add(accountId);
         }
         accountManager.printAccounts();
 
-        accountManager.applyOperations(createOperationListForTest(accountIdList));
+        final int operationNumber = accountNumber * 1;
+        accountManager.applyOperations(createOperationListForTest(accountIdList, operationNumber, transactionLimitTestValue));
     }
 
-    private static List<Operation> createOperationListForTest(Set<Long> accountIdList) {
-        return new ArrayList<>();
+    private static void prepare() {
     }
 
-    private static long generateInitialBalance() {
-        return  (long) (Math.random() * 100000);
+    private static List<Operation> createOperationListForTest(Set<Long> accountIdList, int operationNumber, int transactionLimit) {
+        List<Operation> operations = new ArrayList<>();
+        Random random = new Random();
+        Object[] idArray =  accountIdList.toArray();
+        long accountId;
+        OperationType type;
+        int transactionValue;
+        for (int i = 0; i < operationNumber; i++) {
+            accountId = (long)idArray[random.nextInt(accountIdList.size())];
+            if (accountId % 2 == 0) {
+                type = OperationType.DEPOSIT;
+            } else {
+                type = OperationType.WITHDRAW;
+            }
+            transactionValue = random.nextInt(transactionLimit);
+            operations.add(new Operation(accountId, type, transactionValue));
+        }
+
+        for (Operation operation : operations) {
+            System.out.println(operation);
+        }
+
+        return operations;
+    }
+
+    private static long generateInitialBalance(long limit) {
+        return  (long) (Math.random() * limit);
     }
 }
--- task7-multithreading/src/main/java/subtasks/one/Operation.java
+++ task7-multithreading/src/main/java/subtasks/one/Operation.java
@@ -1,10 +1,19 @@
 package subtasks.one;
 
+import java.util.Objects;
+
 public class Operation {
-    private Operation() {
+    private Long accountId;
+    private OperationType type;
+    private long value;
+
+    Operation(Long accountId, OperationType type, long value) {
+        this.accountId = accountId;
+        this.type = type;
+        this.value = value;
     }
 
-    public static void apply(Account account, OperationType type, long value) {
+    public void apply(Account account) {
         if (type == OperationType.DEPOSIT) {
             account.increase(value);
         } else if (type == OperationType.WITHDRAW) {
@@ -12,4 +21,40 @@ public class Operation {
         }
     }
 
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public OperationType getType() {
+        return type;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operation operation = (Operation) o;
+        return value == operation.value &&
+                Objects.equals(accountId, operation.accountId) &&
+                type == operation.type;
+    }
+
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "accountId=" + accountId +
+                ", type=" + type +
+                ", value=" + value +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(accountId, type, value);
+    }
 }
